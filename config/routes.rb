028e9ab55ef303af278bb79ff0d8cd5820e4ea52
Nguyen Anh Tuan <tuanna.hsp@gmail.com>
2019-08-15 15:50:48 +0900

    Finished chapter 14

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,15 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: %i[new create edit update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,9 +2,24 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get new" do
     get users_new_url
     assert_response :success
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 
 end
